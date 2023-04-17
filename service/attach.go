b92a75bb3cd66617eb0653ae13abeaa28f6f5cb8
Weibo Wang <924228507@qq.com>
2023-04-17 21:49:02 +0800

    finish images send

---
+++ service/attach.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"fmt"
+	"ginchat/utils"
+	"io"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Upload(c *gin.Context) {
+	w := c.Writer
+	req := c.Request
+	srcFile, head, err := req.FormFile("file")
+	if err != nil {
+		utils.RespFail(w, err.Error())
+	}
+	suffix := ".png"
+	ofilName := head.Filename
+	tem := strings.Split(ofilName, ".")
+	if len(tem) > 1 {
+		suffix = "." + tem[len(tem)-1]
+	}
+	fileName := fmt.Sprintf("%d%04d%s", time.Now().Unix(), rand.Int31(), suffix)
+	dstFile, err := os.Create("./asset/upload/" + fileName)
+	if err != nil {
+		utils.RespFail(w, err.Error())
+	}
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		utils.RespFail(w, err.Error())
+	}
+	url := "./asset/upload/" + fileName
+	utils.RespOK(w, url, "发送图片成功")
+}
